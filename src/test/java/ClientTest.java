9ea9cc6a20cebe787fbe79604ce47348fa037215
DJ Roychoudhury <djroychoudhury@yahoo.com>
2016-05-06 12:33:31 -0700

    pass first ClientTest.

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,29 +1,29 @@
-// import org.sql2o.*;
-// import org.junit.*;
-// import static org.junit.Assert.*;
-//
-// public class ClientTest {
-//
-//   @Before
-//   public void setUp() {
-//     DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
-//   }
-//
-//   @After
-//   public void tearDown() {
-//     try(Connection con = DB.sql2o.open()) {
-//       String deleteClientsQuery = "DELETE FROM clients *;";
-//       String deleteStylistsQuery = "DELETE FROM stylists *;";
-//       con.createQuery(deleteClientsQuery).executeUpdate();
-//       con.createQuery(deleteStylistsQuery).executeUpdate();
-//     }
-//   }
-//
-//   @Test
-//   public void Client_instantiatesCorrectly_true() {
-//     Client myClient = new Client("Bill Murray", 1);
-//     assertEquals(true, myClient instanceof Client);
-//   }
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ClientTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteClientsQuery = "DELETE FROM clients *;";
+      String deleteStylistsQuery = "DELETE FROM stylists *;";
+      con.createQuery(deleteClientsQuery).executeUpdate();
+      con.createQuery(deleteStylistsQuery).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Client_instantiatesCorrectly_true() {
+    Client myClient = new Client("Bill Murray", 1);
+    assertEquals(true, myClient instanceof Client);
+  }
 //
 //   @Test
 //   public void getCname_clientInstantiatesWithCname_String() {
@@ -84,5 +84,4 @@
 //     myClient.delete();
 //     assertEquals(null, Client.find(myClientId));
 //   }
-//
-// }
+}
